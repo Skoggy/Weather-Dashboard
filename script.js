6656d854ed333c189dd606c058219acc0567ce5e
Chris Skogstad <chris.skogstad1@gmail.com>
2020-09-12 21:43:48 +1000

    I have the history searches adding to the list inder the search bar, but still am working on getting them to populate without refreshing the page and also turning them into buttons to bring up all the weather data

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
+$(document).ready(function(){
+  //var cityListString = localStorage.getItem("cityList");
+
+  //var cityList = JSON.parse(cityListString);
+
+  //if (cityList == null) {
+    //cityList = {};
+  //}
+  
+  //cityListCreate(cityList)
+
+  //$(".current").hide()
+//})
+
+//function cityListCreate()  {
+  //$(".city-list").empty();
+  //var cityLi = $("<li>")
+
+  
+  //for (var i = 0; i < cityList.length; i++) {
+    //var cityEntry = $("<button>");
+    //cityEntry = $("<li>");
+
+    //var cityInp = $(".city-input").val()
+    //cityEntry.text(cityInp).
+
+    //$(".city-list").append(cityEntry);
+  //}
 
 
-
-
+//}
 
   function clear() {
     $(".current").empty();
   }
   
+
+
+
+let search_history = JSON.parse(localStorage.getItem("cities")) === null ? [] : JSON.parse(localStorage.getItem("cities"));
+
+console.log(search_history)
+
+displayHistory();
+
+
 
 
 //var cityName = $(".city-input").val().trim();
@@ -17,9 +54,26 @@
 $(".submit").on("click", function() {
 
   clear();
+
+
+  if ($(this).attr("id") === "submit")  {
+    city = $(".city-input").val();
+  } else {
+    city = $(this).text();
+  }
+  
+  console.log(city);
+
+  if (search_history.indexOf(city) === -1) {
+    search_history.push(city);
+  }
+
+  console.log(search_history);
+  localStorage.setItem("cities", JSON.stringify(search_history));
+
+
   
   var queryParams = { "APPID": "8c321cc1716884b0a6eec6410a70fa25" }
-
   queryParams.q = $(".city-input").val().trim();
 
   var queryURL = "http://api.openweathermap.org/data/2.5/weather?"
@@ -56,9 +110,35 @@ $(".current").append(cityName)
 $(".current").append("Temperature: ", currentTemp)
 $(".current").append("Humidity: ", humidity)
 $(".current").append("Wind Speed: ", windSpeed)
+
+
+
 }
 
 
+function displayHistory() {
+  $(".city-list").empty();
+  search_history.forEach(function (city) {
+  let historyItem = $("<li>")
 
+  historyItem.text(city);
+
+  $(".city-list").prepend(historyItem);
+  })
+}
+
+
+//$(".submit").on("click", function(event) {
+
+ //var city = $(".city-input").val().trim();
+//if (city != "")  {
+  //cityList[city] = true;
+  //localStorage.setItem("cityList", JSON.stringify(cityList));
+
+  //$(".current").show()
+  
+//}
+
+})
 
 
