281b3dd37c0517265d4874f5c4549bfc8789aefb
Chris Skogstad <chris.skogstad1@gmail.com>
2020-09-13 19:50:43 +1000

    fixed up units and started working on the UV rating

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
 
   let search_history = JSON.parse(localStorage.getItem("cities")) === null ? [] : JSON.parse(localStorage.getItem("cities"));
 
-  console.log(search_history)
+  //console.log(search_history)
 
   displayHistory();
 
@@ -40,9 +40,9 @@ $(document).ready(function () {
       weather = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APPID;
 
       $.getJSON(weather, function (json) {
-        console.log(json)
+        //console.log(json)
 
-        let temp = (json.main.temp - 273.15) * (9 / 5) + 32;
+        let temp = (json.main.temp - 273.15);
         let windspeed = json.wind.speed * 2.237;
         //var cityName = $("<h2>").text(coord.name);
         //var celsTemp = (coord.main.feels_like) - 273.15
@@ -59,13 +59,29 @@ $(document).ready(function () {
         //$(".current").append("Wind Speed: ", windSpeed)
         $("#current-city").text(json.name + " " + current_date);
         $("#weather-img").attr("src", "https://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
-        $("#temperature").text(temp.toFixed(2) + "°F");
+        $("#temperature").text(temp.toFixed(2) + "°C");
         $("#humidity").text(json.main.humidity + "%");
         $("#windspeed").text(windspeed.toFixed(2) + " " + "mph");
-        $()
+        uvRating = $("#uv-rating").text
 
       })
     }
+ 
+    function uvRating()   {
+
+      let lat = json.coord.lat
+      let lon = json.coord.lon
+      console.log(lat)
+      console.log(lon)
+
+      let uvURL = "http://api.openweathermap.org/data/2.5/uvi?" + APPID + "&lat=" + lat + "&lon=" + lon
+
+
+
+    }
+
+
+
 
 function fiveDayForecast() {
   let fiveDayForeCast = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=" + APPID;
@@ -92,7 +108,7 @@ $.ajax({
             let day = date.split("-")[2]
 $("#day-" + dayCount).children(".card-date").text(month + "/" + day + "/" + year)
 $("#day-" + dayCount).children(".weather-icon").attr("src", "https://api.openweathermap.org/img/w/" + response.list[i].weather[0].icon + ".png");
-$("#day-" + dayCount).children(".weather-temp").text("Temp: " + ((response.list[i].main.temp - 273.15) * (9 / 5) + 32).toFixed(2) + "°F");
+$("#day-" + dayCount).children(".weather-temp").text("Temp: " + (response.list[i].main.temp - 273.15).toFixed(2) + "°C");
 $("#day-" + dayCount).children(".weather-humidity").text("Humidity: " + response.list[i].main.humidity + "%");
 dayCount++;
 
@@ -126,134 +142,21 @@ dayCount++;
 
 
 
-    //function formatDates(data) {
-    //var dateArray = data.split("-");
-    //var formattedDate = dateArray[1] + "/" + dateArray[2] + "/" + dateArray[0];
-    //return formattedDate
-    //}
+    
 
 
     $("#submit-city").click(displayHistory);
-    //$(".city-list").on("click", function (event) {
-    // $("li").val($(event.target).text());
-    // historyWeather();
-    //});
+    
   })
-    ////function historyWeather(){ 
-
-
-    //$.ajax({
-    //  url: queryURL,
-    //   method: "GET"
-    // }).then(function(response) {
-    //   console.log(response)
-
-
-    // })  
-    //} 
+    
 
 
 
 
-    //console.log(search_history)
-    //for (var i = 0; i < search_history.length; i++) {
-    // $(".city-list").on('click', function()  {
-    //console.log(search_history)
-    //var queryParams = { "APPID": "8c321cc1716884b0a6eec6410a70fa25" }
-    // console.log(city)
-    //queryParams.q = historyItem
+   
 
 
-    //  var queryURL = "http://api.openweathermap.org/data/2.5/weather?"
-
-    // queryURL = queryURL + $.param(queryParams);
-    // $.ajax({
-    ///   url: queryURL,
-    //   method: "GET"
-    // }).then(function(response) {
-    //  currentWeather(response)
-    //  console.log(response)
-
-
-    /// })
-
-
-    //})
-
-
-
-
-
-    //$(".submit").on("click", function(event) {
-
-    //var city = $(".city-input").val().trim();
-    //if (city != "")  {
-    //cityList[city] = true;
-    //localStorage.setItem("cityList", JSON.stringify(cityList));
-
-    //$(".current").show()
-
-    //}
-    //var queryURL = "http://api.openweathermap.org/data/2.5/weather?"
-    //var forecastURL = "http://api.openweathermap.org/data/2.5/forecast?"
-    //queryURL = queryURL + $.param(queryParams);
-
-
-    //$.ajax({
-    //url: queryURL,
-    //method: "GET"
-    //}).then(function (response) {
-    //currentWeather(response)
-    //console.log(response)
-
-
-    //})})
-
-    //var apiKey = "8c321cc1716884b0a6eec6410a70fa25"
-    //cityName = $(".city-input").val().trim()
-    //var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=" + apiKey
-
-    //forecastURL = forecastURL + $.param(queryParams);
-    //var fiveDayForecastArray = [];
-    //$.ajax({
-    //url: forecastURL,
-    //method: "GET"
-    //}).then(function (response) {
-    //forecastWeather(response)
-    //console.log(response)
-
-
-    // var tempForecastObj;
-
-    //for (var i = 4; i < response.list.length; i += 8) {
-    //tempForecastObj = {
-    //date: response.list[i].dt_text.split(" ")[0],
-    //weatherIcon: response.list[i].weather[0].icon,
-    //temperature: response.list[i].main.temp.toFixed(2),
-    //humidity: response.list[i].main.humidity
-    //};
-    //fiveDayForecastArray.push(tempForecastObj);
-    // }
-
-    //for (var i = 0; i < fiveDayForecastArray.length; i++) {
-    // fiveDayForecastArray[i].date = formatDates(fiveDayForecastArray[i].date);
-    // }
-
-    //var forecastHeader = $("<h5>5-Day Forecast:</h5>");
-    //$("#forecast-header").append(forecastHeader);
-
-    //for (var i = 0; i < fiveDayForecastArray.length; i++) {
-    //var forecast = $('<div class="row"><span class="span"><h5>' + fiveDayForecastArray[i].date + '</h5>' +
-    //'<p><img class="w-100" src="http://openweathermap.org/img/wn/' + fiveDayForecastArray[i].weatherIcon + '@2x.png"></p>' +
-    //'<p>Temp: ' + fiveDayForecastArray[i].temperature + '</p>' +
-    //'<p>Humidity: ' + fiveDayForecastArray[i].humidity + '%</p>' +
-    //'<span></div>');
-    //$(".forecast-row").append(forecast)
-
-    //}
-
-
-    //})}
+   
 
 
 
